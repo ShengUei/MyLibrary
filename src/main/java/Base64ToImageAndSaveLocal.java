52d28cdc150f104a52f314d0f935f26e8c363718
ShengUei <shengueiwengtw@gmail.com>
2022-04-21 20:43:34 +0800

    Initial commit

---
+++ src/main/java/Base64ToImageAndSaveLocal.java
@@ -0,0 +1,25 @@
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+
+public class Base64ToImageAndSaveLocal {
+
+    public String bs64ToImgSave(String imgBase64) {
+        String[] splitByComma = imgBase64.split(",");
+        String fileExtension = splitByComma[0].split("/")[1].split(";")[0];
+        String fileName = String.format("%s.%s", Instant.now().toEpochMilli(), fileExtension);
+        byte[] byteArray = Base64.decodeBase64(splitByComma[1]);
+        try {
+            String pathname = ResourceUtils.getURL("classpath:").getPath() + "static/images/" + fileName;
+            FileUtils.writeByteArrayToFile(new File(pathname), byteArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileName;
+    }
+
+}
